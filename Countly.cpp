ee9edac212386b94dd64ccf32915e92ffb5faf91
lizhiyi <342898089@qq.com>
2014-07-06 17:47:08 -0300

    Update Countly.cpp

--- Countly.cpp
+++ Countly.cpp
@@ -25,7 +25,7 @@ CountlyStore countlyStore;
 boolean thread_status=false;
 static std::mutex mutex;
 
-Countly::Countly()
+Countly::Countly()   
 {
 	queue_ = new ConnectionQueue();	
 
@@ -45,16 +45,18 @@ Countly* Countly::GetInstance()
 	return m_pInstance;  
 }  
 
-void init(string serverUrl, string appKey) 
+void Countly::init(string serverUrl, string appKey)   //初始化
 {
-	void init(string,string,string,string,string);
+	//void init(string,string,string,string,string);
 	init(serverUrl,appKey,NULL,NULL,NULL);	
 }
 
-void init(string serverURL, string appKey, string region, string server, string channel)
+void Countly::init(string serverURL, string appKey, string region, string server, string channel)  //分区、服务器、渠道初始化
 {
+	//此处需设置一个定时器执行onTimer方法
 	//SetTimer(NULL,1,60000,(TIMERPROC)Countly::TimerProc);
-	schedule(schedule_selector(Countly::onTimer), 1);
+	//Countly::m_pInstance->schedule(schedule_selector(Countly::onTimer), 1);  
+	
 	queue_->setServerURL(serverURL);
     queue_->setAppKey(appKey);
 	if (!region.empty()) {
@@ -68,7 +70,7 @@ void init(string serverURL, string appKey, string region, string server, string 
     }
 }
 
-void onTimer(){
+void onTimer(){     //定时器所执行方法，定时更新session状态
     if (isVisible_ == false)
         return;
     double currTime = time(NULL);
@@ -83,7 +85,7 @@ void onTimer(){
         queue_->recordEvents(eventQueue_->events());
 }
 
-void onStart() {
+void onStart() {     //session状态开始
 	void onStartHelper();
     activityCount_++;
     if (activityCount_ == 1){
@@ -91,7 +93,7 @@ void onStart() {
 	}
 }
 
-void onStop() {
+void onStop() {    //session状态结束
 	void onStopHelper();
     activityCount_--;
     if (activityCount_ == 0){
@@ -120,34 +122,34 @@ void onStopHelper() {
     isVisible_ = false;	
 }
 
-void recordEvent(string key){
+void recordEvent(string key){        //事件上报
 	eventQueue_->recordEvent(key);
 	if(eventQueue_->size()>10){
 		queue_->recordEvents(eventQueue_->events());
 	}
 }
-void recordEvent(string key, int count){
+void recordEvent(string key, int count){   //事件上报
 	eventQueue_->recordEvent(key,count);
 	if(eventQueue_->size()>10){
 		queue_->recordEvents(eventQueue_->events());
 	}
 }
 
-void recordEvent(string key, int count, double sum) {
+void recordEvent(string key, int count, double sum) {  //事件上报
 	eventQueue_->recordEvent(key,count,sum);
 	if(eventQueue_->size()>10){
 		queue_->recordEvents(eventQueue_->events());
 	}
 }
 
-void recordEvent(string key, Map<string, string> segmentation, int count){
+void recordEvent(string key, std::map<std::string, std::string> segmentation, int count){  //事件上报
 	eventQueue_->recordEvent(key,segmentation,count);
 	if(eventQueue_->size()>10){
 		queue_->recordEvents(eventQueue_->events());
 	}
 }
 
-void recordEvent(string key, Map<string, string> segmentation, int count, double sum){
+void recordEvent(string key, std::map<std::string, std::string> segmentation, int count, double sum){  //事件上报
 	eventQueue_->recordEvent(key,segmentation,count,sum);
 	if(eventQueue_->size()>10){
 		queue_->recordEvents(eventQueue_->events());
@@ -156,7 +158,7 @@ void recordEvent(string key, Map<string, string> segmentation, int count, double
 
 
 
-void ConnectionQueue::beginSession()
+void ConnectionQueue::beginSession()   //session开始事件
 {
 	string data;
     data = "app_key=" + queue_->getAppKey();
@@ -185,7 +187,7 @@ void ConnectionQueue::beginSession()
     tick();
 }
 
-void ConnectionQueue::updateSession(int duration)
+void ConnectionQueue::updateSession(int duration)  //session更新事件
 {
 	string data;
     data = "app_key=";
@@ -213,7 +215,7 @@ void ConnectionQueue::updateSession(int duration)
     tick();
 }
 
-void ConnectionQueue::endSession(int duration) {
+void ConnectionQueue::endSession(int duration) {    //session结束事件
 	string data;
     data = "app_key=" + queue_->getAppKey();
     data += "&";
@@ -243,7 +245,7 @@ void ConnectionQueue::endSession(int duration) {
 
 }
 
-void ConnectionQueue::recordEvents(string events){
+void ConnectionQueue::recordEvents(string events){    //事件上报数据
 	string data;
 	data = "app_key=" + queue_->getAppKey();
     data += "&";
@@ -269,7 +271,7 @@ void ConnectionQueue::recordEvents(string events){
 }
 
 
-void ConnectionQueue::tick(){	
+void ConnectionQueue::tick(){	//触发事件上报
 
     if (countlyStore.isEmptyConnections())
         return;
@@ -279,7 +281,7 @@ void ConnectionQueue::tick(){
 	}
 }
 
-void Countly::postThread(void){
+void Countly::postThread(void){    //事件上报
 	thread_status = true;
 	while(true){
 		vector<std::string> connections = countlyStore.connections();
@@ -312,16 +314,17 @@ void Countly::requestCompleted(HttpClient *sender, HttpResponse *response)
 
 }
 
-
+//缓存事件大小
 int EventQueue::size(){
 	vector<std::string> events = countlyStore.events();
 	return events.size();
 }
 
-std::string EventQueue::events(){
+std::string EventQueue::events(){     //缓存的事件
 	string result = "";
 	Countly::mutex.lock();
-	list<UserEvent> events = countlyStore.eventsList();
+	list<UserEvent> events;
+	events= countlyStore.eventsList();
 	while(!events.empty()){
 		result += countlyStore.eventToJSON(events.front()).GetString();	
 		events.pop_front();
@@ -332,26 +335,26 @@ std::string EventQueue::events(){
 	return result;
 }
 
-void EventQueue::recordEvent(string key){
+void EventQueue::recordEvent(string key){     //事件放入缓存
 	return EventQueue::recordEvent(key,0);
 }
 
-void EventQueue::recordEvent(string key, int count){
+void EventQueue::recordEvent(string key, int count){   //事件放入缓存
 	return EventQueue::recordEvent(key,count,0.0);
 }
 
-void EventQueue::recordEvent(string key, int count, double sum){
-	 Map<string,string> map;
+void EventQueue::recordEvent(string key, int count, double sum){  //事件放入缓存
+	 std::map<std::string,std::string> map;
 	 return EventQueue::recordEvent(key,map,count,sum);
 }
 
 
-void EventQueue::recordEvent(string key, Map<string, string> segmentation, int count){
+void EventQueue::recordEvent(string key, std::map<std::string, std::string> segmentation, int count){   //事件放入缓存
 	return EventQueue::recordEvent(key,segmentation,count,0);
 }
 
-
-void EventQueue::recordEvent(string key, Map<string, string> segmentation, int count, double sum) {
+//事件放入缓存
+void EventQueue::recordEvent(string key, std::map<std::string, std::string> segmentation, int count, double sum) {
     
     countlyStore.addEvent(key, segmentation, count, sum);
 	//if(eventQueue_->size()>10){
@@ -406,6 +409,7 @@ const char* PREFERENCES = "COUNTLY_STORE";
 const char* DELIMITER = "===";
 const char* CONNECTIONS_PREFERENCE = "CONNECTIONS";
 const char* EVENTS_PREFERENCE = "EVENTS";
+//辅助类，缓存事件
 CountlyStore::CountlyStore()
 {
 	if(!userDefault->getBoolForKey("isExisted"))
@@ -414,9 +418,10 @@ CountlyStore::CountlyStore()
 	}
 }
 
+//session事件
 vector<std::string> CountlyStore::connections()
 {  
-	std:string array = userDefault->getStringForKey(CONNECTIONS_PREFERENCE,NULL);
+	std::string array = userDefault->getStringForKey(CONNECTIONS_PREFERENCE,NULL);
 	if(!array.empty())
 	{
 		vector<std::string> connections = CountlyStore::split(array,DELIMITER);
@@ -424,9 +429,10 @@ vector<std::string> CountlyStore::connections()
 	}
 }
 
+//行为事件
 vector<std::string> CountlyStore::events()
 {
-	std:string array = userDefault->getStringForKey(EVENTS_PREFERENCE,NULL);
+	std::string array = userDefault->getStringForKey(EVENTS_PREFERENCE,NULL);
 	if(!array.empty())
 	{
 		vector<std::string> events = CountlyStore::split(array,DELIMITER);
@@ -434,6 +440,7 @@ vector<std::string> CountlyStore::events()
 	}
 }
 
+//所有行为事件
 list<UserEvent> CountlyStore::eventsList()
 {
 	vector<std::string> eventsVector = CountlyStore::events();
@@ -453,6 +460,7 @@ list<UserEvent> CountlyStore::eventsList()
 	}
 }
 
+//是否有session事件缓存
 boolean CountlyStore::isEmptyConnections(){
 	vector<std::string> connections = countlyStore.connections();
 	if(connections.size()==0){
@@ -461,6 +469,7 @@ boolean CountlyStore::isEmptyConnections(){
 	return false;
 }
 
+//是否有行为事件缓存
 boolean CountlyStore::isEmptyEvents(){
 	if(countlyStore.events().size()==0){
 		return true;
@@ -468,16 +477,17 @@ boolean CountlyStore::isEmptyEvents(){
 	return false;
 }
 
+//缓存session事件
 void CountlyStore::addConnection(std::string str){
-    std:string connections = userDefault->getStringForKey(CONNECTIONS_PREFERENCE,NULL);
+    std::string connections = userDefault->getStringForKey(CONNECTIONS_PREFERENCE,NULL);
 	connections += DELIMITER;
 	connections += str;
 	userDefault->setStringForKey(CONNECTIONS_PREFERENCE,connections);
 }
-
+//清除session事件缓存
 void CountlyStore::removeConnection(std::string str){
 	vector<std::string> connections = countlyStore.connections();
-	std:string value;	
+	std::string value;	
 	for(vector<std::string>::iterator iter=connections.begin();iter!=connections.end();++iter){
 		if(*iter!=str){
 			value += *iter;
@@ -486,7 +496,7 @@ void CountlyStore::removeConnection(std::string str){
 	}	
 	userDefault->setStringForKey(CONNECTIONS_PREFERENCE,value);
 }
-
+//缓存行为事件
 void CountlyStore::addEvent(UserEvent event){
 	list<UserEvent> events = countlyStore.eventsList();
 	boolean contain = false;
@@ -506,7 +516,9 @@ void CountlyStore::addEvent(UserEvent event){
 	userDefault->setStringForKey(EVENTS_PREFERENCE,value);
 }
 
-void CountlyStore::addEvent(std::string key, Map<string, string> segmentation, int count, double sum) {
+//缓存行为事件
+void CountlyStore::addEvent(std::string key, std::map<std::string, std::string> segmentation, int count, double sum) {
+	
 	list<UserEvent> events = countlyStore.eventsList();
 	UserEvent event;	
 	while(!events.empty()){
@@ -520,7 +532,7 @@ void CountlyStore::addEvent(std::string key, Map<string, string> segmentation, i
 		event.key = key;
 		event.count = count;
 		event.sum = sum;
-		event.segmentation=segmentation;
+		//event.segmentation=segmentation;
 		event.timestamp = time(NULL);
 	}
 	else{
@@ -530,12 +542,15 @@ void CountlyStore::addEvent(std::string key, Map<string, string> segmentation, i
 	event.count += count;
 	event.sum += sum;
 	CountlyStore::addEvent(event);
+	
 }
 
+//清除行为事件缓存
 void CountlyStore::removeEvent(UserEvent event){
 	list<UserEvent> events = countlyStore.eventsList();
 	events.remove(event);
 }
+//清除行为事件缓存
 void CountlyStore::removeEvents(list<UserEvent> eventsToRemove) {
 	list<UserEvent> eventsList = countlyStore.eventsList();
 	list<UserEvent>::iterator it;
@@ -544,7 +559,7 @@ void CountlyStore::removeEvents(list<UserEvent> eventsToRemove) {
 	}
 } 
 
-
+//将json数据转为Event事件
 UserEvent CountlyStore::jsonToEvent(rapidjson::Document json)
 {
     UserEvent event;
@@ -555,16 +570,19 @@ UserEvent CountlyStore::jsonToEvent(rapidjson::Document json)
 	if(json.HasMember("segmentation")){
 		rapidjson::Value &segm = json["segmentation"];
 		//if(&segm.IsObject){
-			Map<string,string> segmentation;
+		    
+			std::map<std::string,std::string> segmentation;
 			for (rapidjson::Value::ConstMemberIterator itr = segm.MemberonBegin(); itr != segm.MemberonEnd(); ++itr)
-			{
+			{				
 				segmentation.insert(itr->name.GetString(),itr->value.GetString());
 			}
+			
 		//}		
 	}
 	return event;
 }
 
+//将Event事件转发json格式
 rapidjson::Document CountlyStore::eventToJSON(UserEvent event)
 {
 	rapidjson::Document d;
@@ -578,6 +596,7 @@ rapidjson::Document CountlyStore::eventToJSON(UserEvent event)
 	return d;
 }
 
+//事件比较
 boolean operator == (UserEvent &event1, UserEvent &event2){
 	if(event1.key == event2.key && event1.timestamp == event2.timestamp){
 		return true;
